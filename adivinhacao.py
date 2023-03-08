0d2b7fb4c0bd0bf3e1e178451e0576d0e2e60997
Tay4ang <guilherme-ro1989@hotmail.com>
2023-03-08 15:44:17 -0300

    criando o input de numero

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,3 +1,7 @@
 print ("********************************")
 print ("Bem vindo ao jogo de adivinhação")
 print ("********************************")
+
+numero_secreto = 42
+
+input("Digite o seu numero: ")
