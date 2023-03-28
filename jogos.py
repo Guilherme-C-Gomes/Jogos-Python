7adc5ea0c25d037e0100cb26cf308c94aa1052b4
Tay4ang <guilherme-ro1989@hotmail.com>
2023-03-28 09:59:48 -0300

    Crinado seletor de jogo

--- jogos.py
+++ jogos.py
@@ -2,6 +2,11 @@ print ("********************************")
 print ("********Escolha seu jogo********")
 print ("********************************")
 
+print ("(1) Forca (2) Adivinhação")
 
-
-print ("fim de jogo")
+jogo = int (input("Qual jogo?"))
+            
+if(jogo == 1):
+    print("jogando forca")
+elif(jogo == 2):
+    print("jogando advinhação")
