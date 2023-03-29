b84ba23171e9a2956638c064aa200e8fd9d53784
Tay4ang <guilherme-ro1989@hotmail.com>
2023-03-29 18:50:23 -0300

    if __name__ jogos.py

--- jogos.py
+++ jogos.py
@@ -1,17 +1,21 @@
 import forca
 import adivinhacao
 
-print ("********************************")
-print ("********Escolha seu jogo********")
-print ("********************************")
+def escolher_jogo():
+    print ("********************************")
+    print ("********Escolha seu jogo********")
+    print ("********************************")
 
-print ("(1) Forca (2) Adivinhação")
+    print ("(1) Forca (2) Adivinhação")
 
-jogo = int (input("Qual jogo?"))
-            
-if(jogo == 1):
-    print("jogando forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("jogando advinhação")
-    adivinhacao.jogar()
+    jogo = int (input("Qual jogo?"))
+                
+    if(jogo == 1):
+        print("jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("jogando advinhação")
+        adivinhacao.jogar()
+
+if (__name__ == "__main__");
+    escolher_jogo()
