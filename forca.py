f7ad333f83f835403a787ab0d659136b45f7ff8b
Tay4ang <guilherme-ro1989@hotmail.com>
2023-04-04 16:25:04 -0300

    fazendo o loop de execução do jogo

--- forca.py
+++ forca.py
@@ -3,7 +3,14 @@ def jogar():
     print ("***Bem vindo ao jogo de Forca***")
     print ("********************************")
 
+    palavra_secreta = "banana"
 
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou e não acertou 
+    while(not enforcou and not acertou ):
+        print("jogando...")
 
     print ("fim de jogo")
     
